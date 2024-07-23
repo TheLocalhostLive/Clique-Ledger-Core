6a2deb0eec5e52a707cf7211b14e7ec1bc19b1fc
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-07-23 19:50:24 +0530

    id token parsed and refresh token saved securely.

--- flutter/cliqueledger/lib/models/auth0_id_token.dart
+++ flutter/cliqueledger/lib/models/auth0_id_token.dart
@@ -1,40 +1,57 @@
-// import 'package:json_annotation/json_annotation.dart';
+import 'package:json_annotation/json_annotation.dart';
+part 'auth0_id_token.g.dart';
 
-// part 'auth0_id_token.g.dart';
+@JsonSerializable()
+class Auth0IdToken {
+  Auth0IdToken({
+    required this.nickname,
+    required this.name,
+    required this.email,
+    required this.picture,
+    required this.updateAt,
+    required this.iss,
+    required this.sub,
+    required this.aud,
+    required this.iat,
+    required this.exp,
+    this.authTime,
+  });
 
-// @JsonSerializable()
-// class Auth0IdToken {
-//   Auth0IdToken({
-//     required this.nickname,
-//     required this.name,
-//     required this.email,
-//     required this.picture,
-//     required this.updateAt,
-//     required this.iss,
-//     required this.sub,
-//     required this.aud,
-//     required this.iat,
-//     required this.exp,
-//     this.authTime,
-//   });
+  final String nickname;
+  final String name;
+  final String email;
+  final String picture;
+  @JsonKey(name: 'updated_at')
+  final String updateAt;
+  final String iss;
+  String get userId => sub;
+  final String sub;
+  final String aud;
+  final int iat;
 
-//   final String nickname;
-//   final String name;
-//   final String email;
-//   final String picture;
+  final int exp;
+  @JsonKey(name: 'auth_time')
+  final int? authTime;
 
-//   @JsonKey(name: 'updated_at')
-//   final String updateAt;
+  factory Auth0IdToken.fromJson(Map<String, dynamic> json) =>
+      _$Auth0IdTokenFromJson(json);
 
-//   final String iss;
-//   String get userId => sub;
-//   final String sub;
-  
-//   final String aud;
-//   final String iat;
-//   final String exp;
-//   final String? authTime;
+  Map<String, dynamic> toJson() => _$Auth0IdTokenToJson(this);
 
-//   factory Auth0IdToken.fromJson(Map<String, dynamic> json) => _$Auth0IdTokenFromJson(json);
-//   Map<String, dynamic> toJson() => _$Auth0IdTokenToJson(this);
-// }
+  @override
+  String toString() {
+    return {
+      'nickname': nickname,
+      'name': name,
+      'email': email,
+      'picture': picture,
+      'updateAt': updateAt,
+      'iss': iss,
+      'sub': sub,
+      'aud': aud,
+      'iat': iat,
+      'exp': exp,
+      'authTime': authTime,
+    }.toString();
+  }
+}
--- flutter/cliqueledger/lib/service/authservice.dart
+++ flutter/cliqueledger/lib/service/authservice.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
 
+import 'package:cliqueledger/models/auth0_id_token.dart';
 import 'package:cliqueledger/utility/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_appauth/flutter_appauth.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class _LoginInfo extends ChangeNotifier {
   var _isLoggedIn = false;
@@ -25,7 +28,33 @@ class Authservice {
   final _loginInfo = _LoginInfo();
   get loginInfo => _loginInfo;
 
-  final appAuth = const FlutterAppAuth();
+  final appAuth = FlutterAppAuth();
+  Auth0IdToken? idToken;
+  String? accessToken;
+
+  FlutterSecureStorage? secureStorage;
+
+  bool isAuthResultValid(AuthorizationTokenResponse? result) {
+    return result != null && result.idToken != null;
+  }
+
+  Future<String> _setLocalVariables(AuthorizationTokenResponse? result) async {
+    if (isAuthResultValid(result)) {
+      accessToken = result!.accessToken!;
+      idToken = parseIdToken(result.idToken!);
+      
+
+      if (result.refreshToken != null) {
+        await secureStorage!.write(
+          key: REFRESH_TOKEN_KEY,
+          value: result.refreshToken,
+        );
+      }
+      _loginInfo.isLoggedIn = true;
+      return 'Success';
+    }
+    return 'Something is Wrong!';
+  }
 
   login() async {
     final authorizationTokenRequest = AuthorizationTokenRequest(
@@ -35,6 +64,27 @@ class Authservice {
 
     final result =
         await appAuth.authorizeAndExchangeCode(authorizationTokenRequest);
-    _loginInfo._isLoggedIn = true;
+
+    if (isAuthResultValid(result)) {
+      final auth0IdToken = parseIdToken(result!.idToken!);
+      print(auth0IdToken);
+      _loginInfo._isLoggedIn = true;
+    }
+
+    return _setLocalVariables(result);
+  }
+
+  Auth0IdToken parseIdToken(String idToken) {
+    final parts = idToken.split(r'.');
+
+    final Map<String, dynamic> json = jsonDecode(
+      utf8.decode(
+        base64Url.decode(
+          base64Url.normalize(parts[1]), //body
+        ),
+      ),
+    );
+
+    return Auth0IdToken.fromJson(json);
   }
 }
