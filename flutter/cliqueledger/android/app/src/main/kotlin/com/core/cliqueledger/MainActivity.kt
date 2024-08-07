28c9e87e79f3af960520052540c2f82a9405ac6f
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-08-07 17:50:19 +0530

    Flutter App seperated and deleted from main repo.
    Flutter app have been seperated to another repository to reduce
    technical complications while deploying the server. From this commit
    onwards we will not have any flutter related code in this repo. It
    is moved to https://github.com/CliqueLedger/Clique-Ledger-Flutter.

--- flutter/cliqueledger/android/app/src/main/kotlin/com/core/cliqueledger/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.core.cliqueledger
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- flutter/cliqueledger/lib/utility/routers_constant.dart
+++
@@ -1,9 +0,0 @@
-class RoutersConstants {
-  static const String SIGNUP_PAGE_ROUTE = '/signup';
-  static const String LOGIN_ROUTE = '/login';
-  static const String WELCOME_ROUTE = '/';
-  static const String DASHBOARD = '/dashboard';
-  static const String CLIQUE_ROUTE = '/clique';
-  static const String CLIQUE_SETTINGS_ROUTE = '/clique/settings';
-  static const String ADD_MEMBER_ROUTE = '/cliqeu/settings/add-member';
-}
