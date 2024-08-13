a8c40134bd039fbe331b209e30faa6734505c6b2
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-08-13 23:39:10 +0530

    checkIdentity middleware: user endpoint handling removed from middleware.

--- server/src/middlewares/checkIdentity.ts
+++ server/src/middlewares/checkIdentity.ts
@@ -1,7 +1,5 @@
 
 import type { Request, Response, NextFunction } from "express";
-import {  PrismaClient } from '@prisma/client';
-import generateUserId from "../controllers/generateUserId";
 const AUTH0_DOMAIN = 'dev-1yffugckd6d5gydc.us.auth0.com';
 
 
@@ -20,37 +18,7 @@ export default async function checkIdentity (req: Request, res: Response, next: 
     });
     const userinfojson = await userinfo.json();
     req.body.user = userinfojson;
-
-    //This below code should be handled in Auth0 Post Login action
-    //currently the API is not hosted and to test locally
-    //we are doing it like this.
-
-    //So here we are trying to save the user info in our database
-    //when they are trying to login for the first time
-    //IT IS DONE HERE LIKE THIS JUST FOR TESTING. 
-    
-    const prisma = new PrismaClient();
-    if( typeof userinfojson === 'object' && 
-      userinfojson !== null && "email" in userinfojson 
-      && typeof userinfojson.email === "string" 
-      && "name" in userinfojson
-      && typeof userinfojson.name === "string"
-    )  {
-      const user = await prisma.user.findUnique({
-        where: { mail: userinfojson.email }
-      });
-      console.log(user);
-      if(!user) {
-        
-        await prisma.user.create({data: {
-          user_id: await generateUserId(),
-          user_name: userinfojson.name,
-          mail: userinfojson.email,
-        }});
-
-        console.log("`Created` success!!");
-      }
-    }
+    console.log(userinfojson)
     next();
   } catch(err) {
     console.log("Error");
