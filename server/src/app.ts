e8c42c742e9039c7c5a3bb39f2a4d0d52f133979
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-08-20 02:44:20 +0530

    app.js: socket.io connection and join-room secured.
    Previously code would allow anyone to connect to the socket.io from
    clients like postman or some other custom client that supports
    websocket connection. But now we are validating the token before
    connecting the client to websocket.
    
    Also we are not taking the rooms where the client wants to join
    from the client, rather we are authenticating the user by the
    token and from the authentic user_id we are querying the database
    to get the rooms where the client is allowed to join.

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,6 +5,9 @@ import cliques from './routes/cliques';
 import createTransactionRoute from './routes/transactions';
 import users from './routes/users';
 import ledgers from './routes/ledgers';
+import getUserInfo from './controllers/getUserInfo';
+import * as z from 'zod';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -29,30 +32,50 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello, this is get ...');
 });
 
-type UserSocket = {
-  userId: string;
-  socketId: string;
-};
 
+io.use(async (socket, next) => {
+  const query = socket.handshake.query;
+  const querySchema = z.object({
+    token: z.string(),
+  });
+  let parsedQuery;
+  try {
+    parsedQuery = querySchema.parse(query);
+    const user = await getUserInfo(parsedQuery.token);
+    socket.data.user = user;
+    socketUidMap.set(user.clique_ledger_app_uid, socket.id);
+    next();
+  } catch (err) {
+    console.log('ERROR::socke.io');
+    socket.emit('auth-error', { message: 'Invalid or missing token!!' });
+    console.log(err);
+    return;
+  }
+});
 
 io.on('connection', (socket) => {
   console.log('New user connected:', socket.id);
-  
-  socket.on('join-rooms', (rooms: string) => {
-    console.log(rooms);
-    rooms = JSON.parse(rooms);
-    if (!Array.isArray(rooms) || !rooms.every(room => typeof room === 'string')) {
-      console.error('Invalid rooms data:', rooms);
-      return;
-    }
+
+  socket.on('join-rooms', async () => {
+    const uid = socket.data.user.clique_ledger_app_uid;
+    const prisma = new PrismaClient();
+    const rooms = await prisma.clique.findMany({
+      where: {
+        members: {
+          some: {
+            user_id: uid,
+            is_active: true, 
+          },
+        },
+      },
+    });
 
     rooms.forEach((room) => {
-      socket.join(room);
+      socket.join(room.clique_id);
       console.log(`User joined room: ${room}`);
     });
   });
 
-  
 
   socket.on('disconnect', () => {
     console.log('User disconnected:', socket.id);
